8a322320f7b3191c9a18d627577b0a7b4da1ce13
James Bond <57707960+rcavalero@users.noreply.github.com>
2019-12-07 22:42:22 -0800

    setup initial files.

---
+++ script.js
@@ -0,0 +1,19 @@
+// The app should display standard business hours (9 a.m. to 5 p.m.). 
+// Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present,
+//  or the future. This will change depending on the time of day.
+
+// Each time slot should represent one hour and contain the following:
+    // The time
+    // A field to hold user input
+    // A save button
+
+// Clicking on the save button will store the time and user input in localStorage.
+
+// Near the top of the calendar, the application should display the current day. 
+
+
+// You'll need to use the Moment.js library to work with date and time. 
+// Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+// Minimum Requirements
+
