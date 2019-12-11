5566a789af562d115d816c7dde445be4139e142a
James Bond <57707960+rcavalero@users.noreply.github.com>
2019-12-10 23:27:52 -0800

    updated with required functionality

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# homework5
+# Work Day Scheduler (Homework5)
+
+This application is simple schedule app that allows the user to schedule items in 1 hour time slots from
+9AM to 5PM.  
+
+The app has the following functionality:
+ 1. Creates a new schedule every day.
+ 2. Color codes the hour blocks for past, present & future. (A timer is set to update the colors every minute.)
+ 3. Scheduled items are stored to local storage to allow the user to open & close the app without losing data. 
+
+The application is located here: [Work Day Scheduler](https://rcavalero.github.io/homework5/) 
+
+The application consists of one page:
+![work day scheduler screen](workDayScheduler.png) 
+
+## Built With
+
+* This application was built using HTML, JavaScript, Bootstrap CSS Framework & some additional CSS styling.
+
+## Authors
+
+* **Robert Cavalero** - *Initial work* - [RCavalero](https://github.com/rcavalero)
--- script.js
+++ script.js
@@ -1,19 +1,101 @@
-// The app should display standard business hours (9 a.m. to 5 p.m.). 
-// Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present,
-//  or the future. This will change depending on the time of day.
+$(document).ready(function(){
 
-// Each time slot should represent one hour and contain the following:
-    // The time
-    // A field to hold user input
-    // A save button
+$("#currentDay").text(moment().format("dddd MMMM D, YYYY"));
 
-// Clicking on the save button will store the time and user input in localStorage.
+// this pulls data from local storage if it exists
+var scheduler = JSON.parse(localStorage.getItem("scheduler") || "[]");
 
-// Near the top of the calendar, the application should display the current day. 
+var schedStart = "9:00am";
+var schedEnd = "17:00pm";
 
 
-// You'll need to use the Moment.js library to work with date and time. 
-// Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+// if there is no data in local storage, this creates a new schedule
+if (!localStorage.getItem("scheduler")) {
+  createTimeSlots(schedStart, schedEnd);
+};
 
-// Minimum Requirements
+// when app opens this compares the date in local storage to todays date
+// if dates match it leaves existing data otherwise it creates a new schedule
+var currDate = moment().format('YYYYMMDD');
+var schedulerDate = moment(scheduler[0].timeSlot, 'YYYYMMDDhh:mma');
+var  itemDate = schedulerDate.format('YYYYMMDD');
 
+if ( itemDate !== currDate) {
+  scheduler = [];
+  createTimeSlots(schedStart, schedEnd);
+}; 
+
+// this creates the timeslots and saves them to local storage
+function createTimeSlots(start, end){
+  var timeSlots = [];
+  var startTime = moment(start, 'hh:mm');
+    var endTime = moment(end, 'hh:mm');
+
+   
+    // this creates an array (timeSlots) for each hour from start to end
+    while(startTime <= endTime){
+      timeSlots.push(new moment(startTime).format('YYYYMMDDhh:mma'));
+      startTime.add(1, 'hour');
+    }
+
+    // This takes the timeSlots array and creates the array (scheduler) for user input & local storage
+    timeSlots.forEach(function(timeSlot) {
+      
+    var slot = timeSlot;
+    var momentObj = moment(slot, 'YYYYMMDDhh:mma');
+    var hour = momentObj.format('hA');
+
+    var items = {
+        timeSlot: slot,
+        hour: hour,
+        desc: ""
+    };
+    scheduler.push(items);
+  });
+    localStorage.setItem("scheduler", JSON.stringify(scheduler));
+};
+
+renderItems();
+updateColor();
+
+// this renders the schedule to the screen
+function renderItems(){
+  scheduler.forEach(function(item, index) {
+    $("#hour"+index).text(scheduler[index].hour);
+    $("#desc"+index).val(scheduler[index].desc);
+  })  
+};
+
+// this updates the background color of the text area based on past, present & future
+function updateColor(){
+  scheduler.forEach(function(item, index) {
+    var currHour = moment().format('YYYYMMDDHH');
+    var itemMoment = moment(scheduler[index].timeSlot, 'YYYYMMDDhh:mma');
+    var itemHour = itemMoment.format('YYYYMMDDHH');
+    
+    if (itemHour === currHour) {
+      $("#descBox"+index).attr("class", "col-9 h-100 px-0 present")
+    }
+    else if (itemHour>currHour) {
+      $("#descBox"+index).attr("class", "col-9 h-100 px-0 future")
+    }
+    else if (itemHour<currHour) {
+    $("#descBox"+index).attr("class", "col-9 h-100 px-0 past")
+    }
+  });
+};
+
+
+// this is a timer that updates the background colors every minute
+window.setInterval(updateColor, 60 * 1000);
+
+// this saves data to to the scheduler array and local storage after the user clicks a save button
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+    var element = event.target;
+    var index = element.getAttribute("data-index");
+    scheduler[index].desc = $("#desc"+index).val();
+    localStorage.setItem("scheduler", JSON.stringify(scheduler));
+});
+
+});
