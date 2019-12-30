33c29c2d334195195c124e3cce6c8929f9815bae
James Bond <57707960+rcavalero@users.noreply.github.com>
2019-12-30 13:40:57 -0800

    added dropdowns to select start & end times for schedule.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,97 +2,171 @@ $(document).ready(function(){
 
 $("#currentDay").text(moment().format("dddd MMMM D, YYYY"));
 
-// this pulls data from local storage if it exists
-const scheduler = JSON.parse(localStorage.getItem("scheduler") || "[]");
 
-const schedStart = "9:00am";
-const schedEnd = "17:00pm";
+  let schedStart = "";
+  let schedEnd = "";
 
 
-// if there is no data in local storage, this creates a new schedule
-if (!localStorage.getItem("scheduler")) {
-  createTimeSlots(schedStart, schedEnd);
-};
-
-// when app opens this compares the date in local storage to todays date
-// if dates match it leaves existing data otherwise it creates a new schedule
-const currDate = moment().format('YYYYMMDD');
-const schedulerDate = moment(scheduler[0].timeSlot, 'YYYYMMDDhh:mma');
-const  itemDate = schedulerDate.format('YYYYMMDD');
-
-if ( itemDate !== currDate) {
-  scheduler = [];
-  createTimeSlots(schedStart, schedEnd);
-}; 
+    // this pulls schedule & default data from local storage if it exists
+let dailyTaskDefaults = JSON.parse(localStorage.getItem("dailyTaskDefaults") || "[]");
+let dailyTaskSchedule = JSON.parse(localStorage.getItem("dailyTaskSchedule") || "[]");
 
 // this creates the timeslots and saves them to local storage
 const createTimeSlots = (start, end) => {
-  var timeSlots = [];
-  var startTime = moment(start, 'hh:mm');
-    var endTime = moment(end, 'hh:mm');
-
-   
+  let timeSlots = [];
+  let startTime = moment(start, 'hh:mm');
+    let endTime = moment(end, 'hh:mm');
+    
     // this creates an array (timeSlots) for each hour from start to end
     while(startTime <= endTime){
       timeSlots.push(new moment(startTime).format('YYYYMMDDhh:mma'));
       startTime.add(1, 'hour');
     }
 
-    // This takes the timeSlots array and creates the array (scheduler) for user input & local storage
-    timeSlots.forEach = (timeSlot) => {
-      
-    var slot = timeSlot;
-    var momentObj = moment(slot, 'YYYYMMDDhh:mma');
-    var hour = momentObj.format('hA');
+    // This takes the timeSlots array and creates the array (dailyTaskSchedule) for user input & local storage
+    // timeSlots.forEach = (timeSlot) => {
+    timeSlots.forEach(function(timeSlot) {
 
-    var items = {
+      
+    let slot = timeSlot;
+    let momentObj = moment(slot, 'YYYYMMDDhh:mma');
+    let hour = momentObj.format('ha');
+
+    let items = {
         timeSlot: slot,
         hour: hour,
         desc: ""
     };
-    scheduler.push(items);
-  };
-    localStorage.setItem("scheduler", JSON.stringify(scheduler));
+   dailyTaskSchedule.push(items);
+
+
+  });
+    localStorage.setItem("dailyTaskSchedule", JSON.stringify(dailyTaskSchedule));
 };
 
 const schedule = $("#schedule");
 
-
 // this renders the schedule to the screen
 const renderSchedule = () => {
-  scheduler.forEach(function(item, index) {
-const tsContainer = $(`<div class="container w-100 px-0">`);
-const tsRow = $(`<div class="row w-100">`);
+  dailyTaskSchedule.forEach(function(item, index) {
+  const tsContainer = $(`<div class="container w-100 px-0">`);
+  const tsRow = $(`<div class="row schedRow w-100">`);
 
-const tsCol1 = $(`<div class="col-1 h-100 px-0 text-center hour">`);
-const tsPara = $(`<p id="hour`+index+`" class="pt-2">`);
+  const tsCol1 = $(`<div class="col-1 h-100 px-0 text-center hour">`);
+  const tsPara = $(`<p id="hour`+index+`" class="pt-2">`);
 
-const tsCol2 = $(`<div id="descBox`+index+`">`);  // class is defined in the update color function
-const tsTextArea = $(`<textarea id="desc`+index+`" class="w-100 h-100 pl-2 border-0">`);
+  const tsCol2 = $(`<div id="descBox`+index+`">`);  // class is defined in the update color function
+  const tsTextArea = $(`<textarea id="desc`+index+`" class="w-100 h-100 pl-2 border-0">`);
 
-const tsCol3 = $(`<div class="col-1 h-100 px-0">`);
-const tsButton = $(`<button id="saveBtn`+index+`" data-Index = "`+index+`" type ="submit" class="btn btn-block saveBtn h-100 d-inline"><i class="fa fa-lock"></i></button>`);
+  const tsCol3 = $(`<div class="col-1 h-100 px-0">`);
+  const tsButton = $(`<button id="saveBtn`+index+`" data-Index = "`+index+`" type ="submit" class="btn btn-block saveBtn h-100 d-inline"><i class="fa fa-lock"></i></button>`);
 
-schedule.append(tsContainer);
-tsContainer.append(tsRow);
-tsRow.append(tsCol1, tsCol2, tsCol3);
-tsCol1.append(tsPara);
-tsPara.text(scheduler[index].hour);
-tsCol2.append(tsTextArea);
-tsTextArea.val(scheduler[index].desc);
-tsCol3.append(tsButton);
+  schedule.append(tsContainer);
+  tsContainer.append(tsRow);
+  tsRow.append(tsCol1, tsCol2, tsCol3);
+  tsCol1.append(tsPara);
+  tsPara.text(dailyTaskSchedule[index].hour);
+  tsCol2.append(tsTextArea);
+  tsTextArea.val(dailyTaskSchedule[index].desc);
+  tsCol3.append(tsButton);
 
-})  
+  })  
 };
 
-renderSchedule();
+const startup = () => {
+
+  // if dailyTaskDefaults doesn't exist this will add defaults and set the starting and ending times 
+  if (!localStorage.getItem("dailyTaskDefaults")) {
+    let defaults = {
+      startTime: "9:00am",
+      endTime: "17:00pm",
+      cat1: "A",
+      cat2: "B",
+      cat3: "C"
+  };
+  dailyTaskDefaults.push(defaults);
+
+  localStorage.setItem("dailyTaskDefaults", JSON.stringify(dailyTaskDefaults));
+
+  schedStart = dailyTaskDefaults[0].startTime;
+  schedEnd = dailyTaskDefaults[0].endTime;
+  $("#dayStart").val(dailyTaskDefaults[0].startTime);
+  $("#dayEnd").val(dailyTaskDefaults[0].endTime);
+    }
+
+// This uses start & end times from dailyTaskDefaults if it exists
+    else {
+    
+      schedStart = dailyTaskDefaults[0].startTime;
+      schedEnd = dailyTaskDefaults[0].endTime;
+      $("#dayStart").val(dailyTaskDefaults[0].startTime);
+      $("#dayEnd").val(dailyTaskDefaults[0].endTime);
+
+
+    };
+
+  
+// if there is no data in dailyTaskSchedule in local storage, this creates a new schedule
+  if (!localStorage.getItem("dailyTaskSchedule")) {
+    createTimeSlots(schedStart, schedEnd);
+    renderSchedule();
+  }
+  else {useExistingData();
+  };
+};
+
+const useExistingData = ()  => {
+  // this compares the date in local storage to todays date
+    // if dates match it leaves existing data otherwise it creates a new schedule
+    const currDate = moment().format('YYYYMMDD');
+    const scheduleDate = moment(dailyTaskSchedule[0].timeSlot, 'YYYYMMDDhh:mma');
+    const  itemDate = scheduleDate.format('YYYYMMDD');
+
+  if ( itemDate !== currDate) {
+      dailyTaskSchedule = [];
+      
+      createTimeSlots(schedStart, schedEnd);
+      renderSchedule();
+  } 
+  else {
+    renderSchedule();
+  } 
+};
+
+
+startup();
+
+// this remakes the schedule based on the starting & ending times selected by the user
+// and updates the selected values to local storage so they will be used next time app is opened
+$("#newSched").on("click", function(event) {
+  event.preventDefault();
+  schedStart = $("#dayStart").val();
+  schedEnd = $("#dayEnd").val();
+  dailyTaskDefaults[0].startTime = $("#dayStart").val();
+  dailyTaskDefaults[0].endTime = $("#dayEnd").val();
+  localStorage.setItem("dailyTaskDefaults", JSON.stringify(dailyTaskDefaults));
+
+  schedule.empty();
+  dailyTaskSchedule = [];
+  createTimeSlots(schedStart, schedEnd);
+  renderSchedule();
+  updateColor();
+  
+});  
+
+// $("#dayEnd").on("change", function(event){
+//   event.preventDefault();
+//   $("#dayEnd").children().removeAttr("selected");
+//   $("#dayEnd option:selected").attr("selected", "selected");
+
+// }) 
 
 
 // this updates the background color of the text area based on past, present & future
 const updateColor = () => {
-  scheduler.forEach(function(item, index) {
+  dailyTaskSchedule.forEach(function(item, index) {
     const currHour = moment().format('YYYYMMDDHH');
-    const itemMoment = moment(scheduler[index].timeSlot, 'YYYYMMDDhh:mma');
+    const itemMoment = moment(dailyTaskSchedule[index].timeSlot, 'YYYYMMDDhh:mma');
     const itemHour = itemMoment.format('YYYYMMDDHH');
     
     if (itemHour === currHour) {
@@ -112,13 +186,13 @@ updateColor();
 // this is a timer that updates the background colors every minute
 window.setInterval(updateColor, 60 * 1000);
 
-// this saves data to to the scheduler array and local storage after the user clicks a save button
+// this saves data to to the dailyTaskSchedule array and local storage after the user clicks a save button
 $(".saveBtn").on("click", function(event) {
   event.preventDefault();
-    var element = event.target;
-    var index = element.getAttribute("data-index");
-    scheduler[index].desc = $("#desc"+index).val();
-    localStorage.setItem("scheduler", JSON.stringify(scheduler));
+    let element = event.target;
+    let index = element.getAttribute("data-index");
+    dailyTaskSchedule[index].desc = $("#desc"+index).val();
+    localStorage.setItem("dailyTaskSchedule", JSON.stringify(dailyTaskSchedule));
 });
 
 });
